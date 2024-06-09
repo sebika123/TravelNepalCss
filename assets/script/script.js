f35deedc73d4dc4b8173f4425943868485fcdf07
sebika nepal <72345247+sebika123@users.noreply.github.com>
2024-06-09 19:46:05 +0545

    review changes done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,10 +1,20 @@
+// function toggleNav() {
+//   const overlay = document.getElementById("nav-overlay");
+//   overlay.style.display = overlay.style.display === "flex" ? "none" : "flex";
+// }
+
+// function toggleSubMenu(event, submenuId) {
+//     event.preventDefault();
+//   const submenu = document.getElementById(submenuId);
+//   submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+// }
 function toggleNav() {
   const overlay = document.getElementById("nav-overlay");
   overlay.style.display = overlay.style.display === "flex" ? "none" : "flex";
 }
 
 function toggleSubMenu(event, submenuId) {
-    event.preventDefault();
+  event.stopPropagation();
   const submenu = document.getElementById(submenuId);
   submenu.style.display = submenu.style.display === "block" ? "none" : "block";
 }
